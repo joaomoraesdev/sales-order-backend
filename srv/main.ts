847f05f83cb98cdc1e2d1a7cafb1500306f0f838
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-02-03 15:45:27 -0300

    feat: add after read customer logic

--- srv/main.ts
+++ srv/main.ts
@@ -1,2 +1,13 @@
-const funcao = (variavel: string) => console.log(variavel);
-funcao('123');
+import { Service } from "@sap/cds";
+import { Customers } from '@models/sales';
+import { getMaxListeners } from "node:cluster";
+
+export default (service: Service) => {
+    service.after('READ', 'Customers', (results: Customers) => {
+        results.forEach(customer => {
+            if(!customer.email?.includes('@')) {
+                customer.email = `${customer.email}@gmail.com`;
+            }
+        })
+    });
+}
