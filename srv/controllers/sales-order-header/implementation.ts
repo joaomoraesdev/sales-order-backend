860f9dce4cd053b7c5090d71a0addb6b9f7827b8
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-02-13 17:58:44 -0300

    feat: add afterCreate in SalesOrderHeaderController and SalesOrderHeaderControllerImpl

--- srv/controllers/sales-order-header/implementation.ts
+++ srv/controllers/sales-order-header/implementation.ts
@@ -1,11 +1,16 @@
-import { SalesOrderHeader } from "@models/sales";
+import { SalesOrderHeader, SalesOrderHeaders } from "@models/sales";
 import { CreationPayloadValidationResult, SalesOrderHeaderController } from "./protocols";
 import { SalesOrderHeaderService } from "srv/services/sales-order-header/protocols";
+import { User } from "@sap/cds";
 
 export class SalesOrderHeaderControllerImpl implements SalesOrderHeaderController {
     constructor(private readonly service: SalesOrderHeaderService) {}
     
     public async beforeCreate(params: SalesOrderHeader): Promise<CreationPayloadValidationResult> {
         return this.service.beforeCreate(params);
+    }
+
+    afterCreate(params: SalesOrderHeaders, loggedUser: User): Promise<void> {
+        return this.service.afterCreate(params, loggedUser);
     }
 }
