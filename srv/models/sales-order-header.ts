4c89714afd865da110940db88c00e27cc7111110
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-02-13 17:56:10 -0300

    feat: add getProductsData, toStringfiedObject, with methods and adjust types

--- srv/models/sales-order-header.ts
+++ srv/models/sales-order-header.ts
@@ -8,7 +8,7 @@ type SalesOrderHeaderProps = {
     totalAmount: number;
 }
 
-type SalesOrderHeaderPropsWithoutTotalAmount = Omit<SalesOrderHeaderProps, 'id' | 'totalAmount'>;
+type SalesOrderHeaderPropsWithoutIdAndTotalAmount = Omit<SalesOrderHeaderProps, 'id' | 'totalAmount'>;
 
 type CreationPayload = {
     customer_id: SalesOrderHeaderProps['customerId'];
@@ -23,12 +23,16 @@ type CreationPayloadValidationResult = {
 export class SalesOrderHeaderModel {
     constructor(private props: SalesOrderHeaderProps) { }
 
-    public static create(props: SalesOrderHeaderPropsWithoutTotalAmount): SalesOrderHeaderModel {
+    public static create(props: SalesOrderHeaderPropsWithoutIdAndTotalAmount): SalesOrderHeaderModel {
         return new SalesOrderHeaderModel({
             ...props,
             id: crypto.randomUUID(),
             totalAmount: 0
         });
+    }
+
+    public static with(props: SalesOrderHeaderProps): SalesOrderHeaderModel {
+        return new SalesOrderHeaderModel(props);
     }
 
     public get id() {
@@ -79,6 +83,17 @@ export class SalesOrderHeaderModel {
             totalAmount -= totalAmount * 0.1
         }
         return totalAmount;
+    }
+
+    public getProductsData(): { id: string, quantity: number}[] {
+        return this.items.map(item => ({
+            id: item.productId,
+            quantity: item.quantity
+        }));
+    }
+
+    public toStringfiedObject(): string {
+        return JSON.stringify(this.props);
     }
 
     private validateCustomerOnCreation(customerId: CreationPayload['customer_id']): CreationPayloadValidationResult {
