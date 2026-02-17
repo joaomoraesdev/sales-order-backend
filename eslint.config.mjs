3ebdb7d13c251e218241d82374a043973904ab00
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-02-17 20:12:57 -0300

    feat: add eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,49 @@
-import cds from '@sap/cds/eslint.config.mjs'
-export default [ ...cds.recommended ]
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+
+export default [
+    {
+        languageOptions: {
+            globals: globals.node
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: ['./gen/*.{js, ts}'],
+        files: ['**/*.{mjs, js, ts}'],
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    caughtErros: 'all',
+                    caughtErrosIgnorePattern: '^ignore',
+                    ignoreRestSiblings: true
+                }
+            ],
+            'eol-last': 'error',
+            indent: [
+                'error',
+                4,
+                {
+                    SwitchCase: 1
+                }
+            ],
+            'max-len': ['error', 120],
+            'max-lines-per-function': ['error', 30],
+            'object-curly-spacing': ['error', 'always'],
+            quotes: ['error', 'single'],
+            'quote-props': ['error', 'as-needed'],
+            semi: ['error', 'always'],
+            'sort-imports': [
+                'error',
+                {
+                    memberSyntaxSortOder: ['single', 'all', 'multiple', 'none'],
+                    allowSeparatedGroups: true
+                }
+            ]
+        }
+    }
+]
