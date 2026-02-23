8061599c4dc253ed5617209fd63cf422ed4a1c09
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-02-23 16:59:15 -0300

    feat: add findByCustomerId on SalesReportServiceImpl and SalesReportService

--- srv/services/sales-report/implementation.ts
+++ srv/services/sales-report/implementation.ts
@@ -13,4 +13,11 @@ export class SalesReportServiceImpl implements SalesReportService {
         return reportData.map(r => r.toObject());
     }
 
+    public async findByCustomerId(customerId: string): Promise<SalesReportByDays[]> {
+        const reportData = await this.repository.findByCustomerId(customerId);
+        if(!reportData)
+            return [];
+
+        return reportData.map(r => r.toObject());
+    }
 }
