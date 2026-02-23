97ef404c6b8b228760f2b8217c26b32101f4aed0
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-02-23 16:59:57 -0300

    feat: add findByCustomerId method in SalesReportControllerImpl and SalesReportController

--- srv/controllers/sales-report/implementation.ts
+++ srv/controllers/sales-report/implementation.ts
@@ -1,12 +1,15 @@
-import { ExpectedResult as SalesReportByDays } from "@models/db/types/SalesReportByDays";
+import { ExpectedResult as SalesReport } from "@models/db/types/SalesReportByDays";
 import { SalesReportController } from "./protocols";
 import { SalesReportService } from "srv/services/sales-report/protocols";
 
 export class SalesReportControllerImpl implements SalesReportController {
     constructor(private readonly service: SalesReportService) { }
 
-    public async findByDays(days: number): Promise<SalesReportByDays[]> {
+    public async findByDays(days: number): Promise<SalesReport[]> {
         return this.service.findByDays(days);
     }
 
+    public async findByCustomerId(customerId: string): Promise<SalesReport[]> {
+        return this.service.findByCustomerId(customerId);
+    }
 }
