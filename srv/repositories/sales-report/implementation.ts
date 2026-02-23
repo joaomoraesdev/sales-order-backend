b92eeb7258dfa156a72888d24d356095cf117c5b
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-02-23 16:58:53 -0300

    feat: add findByCustomerId method in SalesReportRepository and SalesReportRepositoryImpl

--- srv/repositories/sales-report/implementation.ts
+++ srv/repositories/sales-report/implementation.ts
@@ -6,24 +6,35 @@ import { SalesReportModel } from 'srv/models/sales-report';
 import { SalesReportRepository } from './protocols';
 
 export class SalesReportRepositoryImpl implements SalesReportRepository {
+    
     /* eslint-disable-next-line max-lines-per-function */
     public async findByDays(days: number): Promise<SalesReportModel[] | null> {
         const today = new Date().toISOString();
         const subtractedDays = new Date();
         subtractedDays.setDate(subtractedDays.getDate() - days);
         const subtractedDaysISOString = subtractedDays.toISOString();
-
-        const sql = SELECT.from('sales.SalesOrderHeaders')
-            .columns(
-                'id as salesOrderId',
-                'totalAmount as salesOrderTotalAmount',
-                'customer.id as customerId',
-                `customer.firstName || ' ' || customer.lastName as customerFullname`
-            )
-            .where({ createdAt: { between: subtractedDaysISOString, and: today } });
-
+        
+        const sql = this.getReportBaseSql().where({ createdAt: { between: subtractedDaysISOString, and: today } }); 
         const salesReports = await cds.run(sql);
+        return this.mapReportResult(salesReports);
+    }
 
+    public async findByCustomerId(customerId: string): Promise<SalesReportModel[] | null> {
+        const sql = this.getReportBaseSql().where({ customer_id: customerId });
+        const salesReports = await cds.run(sql);
+        return this.mapReportResult(salesReports);
+    }
+    private getReportBaseSql(): any {
+        return SELECT.from('sales.SalesOrderHeaders')
+        .columns(
+            'id as salesOrderId',
+            'totalAmount as salesOrderTotalAmount',
+            'customer.id as customerId',
+            `customer.firstName || ' ' || customer.lastName as customerFullname`
+        );
+    }
+
+    private mapReportResult(salesReports: SalesReportByDays[]): SalesReportModel[] | null {
         if (salesReports.length === 0) return null;
         
         return salesReports.map((salesReport: SalesReportByDays) =>
