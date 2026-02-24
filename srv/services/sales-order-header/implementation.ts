008d852aaa23e7678d0e0ba20d93f87a2df98a9d
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-02-24 17:28:44 -0300

    feat: add return result with headers in bulkCreate action

--- srv/services/sales-order-header/implementation.ts
+++ srv/services/sales-order-header/implementation.ts
@@ -90,9 +90,14 @@ export class SalesOrderHeaderServiceImpl implements SalesOrderHeaderService {
         await this.salesOrderHeaderRepository.bulkCreate(bulkCreateHeaders);
         await this.afterCreate(headers, loggedUser);
 
+        return this.serializeBulkCreateResult(bulkCreateHeaders);
+    }
+
+    private serializeBulkCreateResult(headers: SalesOrderHeaderModel[]): CreationPayloadValidationResult {
         return {
-            hasError: false
-        }
+            hasError: false,
+            headers: headers.map(header => header.toCreationObject())
+        };
     }
 
     private async validateProductOnCreation(header: SalesOrderHeader | BulkCreateSalesOrderPayload): Promise<CreationPayloadValidationResult> {
