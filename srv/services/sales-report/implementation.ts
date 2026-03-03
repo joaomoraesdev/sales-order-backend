f257b2b3768e029bf1ff651f22c8f52083ead064
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-03-03 11:03:56 -0300

    feat: add EitherMonad approarch in SalesReportService

--- srv/services/sales-report/implementation.ts
+++ srv/services/sales-report/implementation.ts
@@ -1,15 +1,25 @@
 import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReportByDays';
 import { SalesReportService } from './protocols';
 import { SalesReportRepository } from 'srv/repositories/sales-report/protocols';
+import { Either, left, right } from '@sweet-monads/either';
+import { AbstractError, NotFoundError, ServerError } from 'srv/errors';
 
 export class SalesReportServiceImpl implements SalesReportService {
     constructor(private readonly repository: SalesReportRepository) {}
 
-    public async findByDays(days: number): Promise<SalesReportByDays[]> {
-        const reportData = await this.repository.findByDays(days);
-        if (!reportData) return [];
+    public async findByDays(days: number): Promise<Either<AbstractError, SalesReportByDays[]>> {
+        try {
+            const reportData = await this.repository.findByDays(days);
+            const stack = new Error().stack as string;
+            if (!reportData)
+                return left(new NotFoundError('Nenhum dado encontrado para os parâmetros informados.', stack));
 
-        return reportData.map((r) => r.toObject());
+            const mappedData = reportData.map((r) => r.toObject());
+            return right(mappedData);
+        } catch (error) {
+            const errorInstance = error as Error;
+            return left(new ServerError(errorInstance.stack as string, errorInstance.message));
+        }
     }
 
     public async findByCustomerId(customerId: string): Promise<SalesReportByDays[]> {
--- srv/services/sales-report/protocols.ts
+++ srv/services/sales-report/protocols.ts
@@ -1,6 +1,8 @@
 import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReportByDays';
+import { Either } from '@sweet-monads/either';
+import { AbstractError } from 'srv/errors';
 
 export interface SalesReportService {
-    findByDays(days: number): Promise<SalesReportByDays[]>;
+    findByDays(days: number): Promise<Either<AbstractError, SalesReportByDays[]>>;
     findByCustomerId(customerId: string): Promise<SalesReportByDays[]>;
 }
