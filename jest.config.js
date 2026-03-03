b60ee2eef6779d97bee217a8cd2211a20a429b89
João Antonio de Brito Moraes <jmoraes2003@hotmail.com>
2026-03-03 18:12:12 -0300

    chore: add jest dependencies

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+    preset: 'ts-jest',
+    collectCoverage: false,
+    globalSetup: './test/e2e/config/setup.ts',
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+        prefix: '<rootDir>/'
+    }),
+    testMatch: ['**/*.e2e.ts']
+};
